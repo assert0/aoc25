79f6c7ac0e4fec5fc00e8f62fd8912fa293138ee
assert0 <assert0@github.com>
2025-12-02 00:03:06 -0500

    Day 1

---
+++ src/days/day1.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+pub fn day1(args: &[String]) {
+    println!("Day 1");
+    if args.len() != 1 {
+        println!("Missing input file");
+        return;
+    }
+    let filename = &args[0];
+    println!("In file {}", filename);
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let rotations: Vec<i32> = contents
+        .lines()
+        .map(|l| {
+            let v = l[1..].parse::<i32>().unwrap();
+            match l[..1].as_ref() {
+                "L" => -v,
+                "R" => v,
+                _ => unreachable!(),
+            }
+        })
+        .collect();
+
+    let mut pointing = 50;
+    let part1 = rotations.iter()
+        .map(|r| {
+            pointing += r;
+            pointing
+        })
+        .filter(|p| p % 100 == 0)
+        .count();
+    println!("Part 1: {}", part1);
+
+    let mut part2 = 0;
+    pointing = 50;
+    for r in rotations {
+        for _ in 0..r.abs() {
+            if pointing == 0 {
+                part2 += 1;
+            }
+            pointing += if r < 0 { 99 } else { 1 };
+            pointing %= 100;
+        } 
+    }
+    println!("Part 2: {}", part2);
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![recursion_limit = "64"]
+
+use std::env;
+use aoc25::days;
+
+fn main() {
+    println!("Advent of Code 2025");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Missing input day");
+        std::process::exit(1);
+    }
+    let (day, dayargs) = (&args[1], &args[2..]);
+    match day.as_ref() {
+        "day1" => days::day1::day1(dayargs),
+        // "day2" => days::day2::day2(dayargs),
+        // "day3" => days::day3::day3(dayargs),
+        // "day4" => days::day4::day4(dayargs),
+        // "day5" => days::day5::day5(dayargs),
+        // "day6" => days::day6::day6(dayargs),
+        // "day7" => days::day7::day7(dayargs),
+        // "day8" => days::day8::day8(dayargs),
+        // "day9" => days::day9::day9(dayargs),
+        // "day10" => days::day10::day10(dayargs),
+        // "day11" => days::day11::day11(dayargs),
+        // "day12" => days::day12::day12(dayargs),
+        _ => {
+            println!("Unknown day: {}", day);
+        }
+    };
+}
